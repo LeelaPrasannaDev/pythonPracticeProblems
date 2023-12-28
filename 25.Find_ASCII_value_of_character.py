9de3f7443c9cd211eedad8ed415118c163496081
setty leela prasanna venkateswara <140870023+LeelaPrasannaDev@users.noreply.github.com>
2023-12-28 20:56:08 +0530

    to find ASCII values by using buildin function ord()

---
+++ 25.Find_ASCII_value_of_character.py
@@ -0,0 +1,9 @@
+#ASCII = Amarican standered code for information interchange
+# There are 128 characters including numbers, alphabets(upper and lower),symboles,special characters
+# build in function of ASCII = ord()
+
+# Take user input to find the value of ASCII
+
+char = input("Enter a charector to find the ASCII value = ")
+
+print('The value of {} in ASCII is = {}'.format(char,ord(char)))
