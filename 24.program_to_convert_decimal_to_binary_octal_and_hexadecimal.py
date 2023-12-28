a660ddc24cf3ff3b539b8a56fa74657a59d20960
setty leela prasanna venkateswara <140870023+LeelaPrasannaDev@users.noreply.github.com>
2023-12-28 20:45:20 +0530

    covert decimal to binary,octal,hexadecimal

---
+++ 24.program_to_convert_decimal_to_binary_octal_and_hexadecimal.py
@@ -0,0 +1,15 @@
+# prifix of binary = 0b 
+# build in function for binary bin()
+# prifix of octal = 0o
+# build in function for octal oct()
+# prifix of hexadecimal = 0x
+# build in function for hexadecimal hex()
+
+# use build in fuctions to convert decimal to above three forms
+
+# take user input of decimal number 
+
+decimal = int(input("Enter a decimal number = "))
+print(bin(decimal),"in binary form")
+print(oct(decimal),"in octal form")
+print(hex(decimal),"in hexadecimal")
