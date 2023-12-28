399355911cd1ea77cec89d38cd581893656113c5
setty leela prasanna venkateswara <140870023+LeelaPrasannaDev@users.noreply.github.com>
2023-12-28 19:24:21 +0530

    display power of 2 using lambda function upto given power for example if user enter 3 then 2**1,2**2,2**3

---
+++ DisplayPowersOf2UsingAnonymousFunction.py
@@ -0,0 +1,12 @@
+# lambda fuction 
+# if user give 5 then 2**0, 2**1, 2**2, 2**3, 2**4, 2**5
+
+nterms = int(input("Enter a term here = "))
+
+def power2(a):
+    result = list(map(lambda x : 2**x, range(a+1)))
+    return result
+
+re = power2(nterms)
+for i in range(1,nterms+1):
+    print(" The value of 2 power {} is {}".format(i,re[i]))
