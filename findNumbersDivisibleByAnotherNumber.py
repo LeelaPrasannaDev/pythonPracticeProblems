887e333d0eefb3c014480283df8ac1be3034e5f0
setty leela prasanna venkateswara <140870023+LeelaPrasannaDev@users.noreply.github.com>
2023-12-28 20:06:04 +0530

    find numbers which is divisible by targeted number

---
+++ findNumbersDivisibleByAnotherNumber.py
@@ -0,0 +1,34 @@
+#if user give a range from 1 to 100 and also check number assume 13
+# we need to check how many numbers in 1 to 100 is divisable by 13 and gives reminder 0 
+
+'''
+To slove this 
+
+1. take user input of starting number in the range 
+2. take another user input of ending number in the range 
+3. and also take user input that which number we need to check if the range values is divisable by
+4.by using for loop take values one by one in the range 
+5. apply condition where the number is divisible by check number and gives reminder zero then it is divisible 
+'''
+
+# code start from here
+
+start = int(input("Enter a starting number to start the range = "))
+end = int(input("Enter end number to end the range = "))
+target_number = int(input("Enter a number you need to check range is divisible by = "))
+#starting a function where the logic return
+def check_divisable(a,b,c):
+   # taking empty list to store values
+    values=[]
+     # take forloop to acces all the numbers in the range
+    for i in range(a,b):
+        # condition to check divisible by check number
+        if i%c==0:
+            values.append(i)
+        else:
+            continue
+    return values
+#calling function
+        
+result = check_divisable(start,end,target_number)
+print(" The numbers in range from {} to {}, which are divisible by {} are = {}".format(start,end,target_number,result))
